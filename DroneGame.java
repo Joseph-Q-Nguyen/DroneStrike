0974a8135ef11c9cca12d116bdf351d3c770ebf4
micompany4 <42653786+micompany4@users.noreply.github.com>
2018-10-27 11:36:35 -0700

    Update DroneGame.java

---
+++ DroneGame.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class DroneGame 
+{
+	public static void main(String[] args)
+	{
+		JFrame screen = new JFrame("Drone Strike");
+		
+		
+		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		screen.setSize(1000, 500);
+		screen.setVisible(true);
+	}
+}
