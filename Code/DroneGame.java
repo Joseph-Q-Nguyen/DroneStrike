c27038633e190e9ed3ad382f02df07ac257c4847
micompany4 <michael.j.wong@sjsu.edu>
2018-11-10 11:50:19 -0800

    Update Code/DroneGame.java
    The key listeners are working, but there seems to be an invisible drone and the commands are only affecting the invisible one. Not sure why there's an invisible one, but either way we need to get ride of that one and debug the movements.

--- Code/DroneGame.java
+++ Code/DroneGame.java
@@ -1,27 +1,93 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+import java.util.Random;
 import javax.swing.*;
 
 
 // Joseph's note: anything added to be displayed on screen must be added to the Moving Plain
-public class DroneGame 
+public class DroneGame //implements KeyListener
 {
 	public final static int GAME_WIDTH = 1000;
 	public final static int GAME_HEIGHT = 500;
+	private Random r;
+	private int x;
 	
-	public static void main(String[] args)
+	public DroneGame() throws IOException
 	{
 		JFrame screen = new JFrame("Drone Strike");
-		
-		Airplane f15 = new Airplane(500, 250, 150, 25);
-		JLabel airplane = new JLabel(f15);
 		MovingPlain plainBackground = new MovingPlain();
+		r = new Random();
+		
+		Airplane f15 = new Airplane(750, r.nextInt(450), 150, 25);
+		JLabel airplane = new JLabel(f15);
+		Airplane tomcat = new Airplane(800, r.nextInt(450), 150, 25);
+		JLabel airplane2 = new JLabel(tomcat);
+		Drone drone = new Drone(200, 100, 150, 25);
+		JLabel label = new JLabel(drone);
+
+		KeyListener f = new KeyListener()
+		{
+
+			@Override
+			public void keyPressed(KeyEvent event) 
+			{
+				// TODO Auto-generated method stub
+				if(event.getKeyCode() == KeyEvent.VK_UP)
+				{
+					System.out.println("up");
+					label.setText("Pull Up!");
+					label.setLocation((int)label.getAlignmentX(), label.getY()-1);
+					
+					label.repaint();
+					
+				}
+				if(event.getKeyCode() == KeyEvent.VK_DOWN)
+				{
+					System.out.println("down");
+					label.setText("dive! dive! dive!");
+					
+					label.setLocation((int)label.getAlignmentX(), label.getY()+1);
+					label.repaint();
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) 
+			{
+				// TODO Auto-generated method stub
+				System.out.println("Done");
+			}
+
+			@Override
+			public void keyTyped(KeyEvent event) 
+			{
+				// TODO Auto-generated method stub
+				
+			}
+	
+		};
 		
 		screen.add(plainBackground);
+		screen.addKeyListener(f);
+		label.requestFocus();
 		
+		
+		plainBackground.add(label);
 		plainBackground.add(airplane);
-		
+		plainBackground.add(airplane2);
+				
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		screen.setSize(GAME_WIDTH , GAME_HEIGHT);
 		screen.setVisible(true);
-	}	
+	}
+	
+	
+	public static void main(String[] args) throws IOException 
+	{
+		DroneGame persona5 = new DroneGame();
+	}
+
+
 }
