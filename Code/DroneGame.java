3a92f815ab12d0791d324e45a56a0a8a3809ecbd
micompany4 <michael.j.wong@sjsu.edu>
2018-11-16 19:55:06 -0800

    Update Code/DroneGame.java

--- Code/DroneGame.java
+++ Code/DroneGame.java
@@ -1,16 +1,8 @@
 import java.awt.*;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.util.Random;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import javax.swing.*;
 
 
@@ -22,81 +14,23 @@ public class DroneGame //implements KeyListener
 	private Random r;
 	private int x;
 	
-
 	public DroneGame() throws IOException
 	{
 		JFrame screen = new JFrame("Drone Strike");
 		MovingPlain plainBackground = new MovingPlain();
 		r = new Random();
-
-	public static void main(String[] args) throws IOException
-	{
-		JFrame screen = new JFrame("Drone Strike");
-		MovingPlain plainBackground = new MovingPlain();
-
 		
 		Airplane f15 = new Airplane(750, r.nextInt(450), 150, 25);
 		JLabel airplane = new JLabel(f15);
-
 		Airplane tomcat = new Airplane(800, r.nextInt(450), 150, 25);
 		JLabel airplane2 = new JLabel(tomcat);
-		Drone drone = new Drone(200, 100, 150, 25);
-		JLabel label = new JLabel(drone);
-
-		KeyListener f = new KeyListener()
-		{
-
-			@Override
-			public void keyPressed(KeyEvent event) 
-			{
-				// TODO Auto-generated method stub
-				if(event.getKeyCode() == KeyEvent.VK_UP)
-				{
-					System.out.println("up");
-					label.setText("Pull Up!");
-					label.setLocation((int)label.getAlignmentX(), label.getY()-1);
-					
-					label.repaint();
-					
-				}
-				if(event.getKeyCode() == KeyEvent.VK_DOWN)
-				{
-					System.out.println("down");
-					label.setText("dive! dive! dive!");
-					
-					label.setLocation((int)label.getAlignmentX(), label.getY()+1);
-					label.repaint();
-				}
-			}
-
-			@Override
-			public void keyReleased(KeyEvent arg0) 
-			{
-				// TODO Auto-generated method stub
-				System.out.println("Done");
-			}
-
-			@Override
-			public void keyTyped(KeyEvent event) 
-			{
-				// TODO Auto-generated method stub
-				
-			}
-	
-		};
-
 		
-		Drone drone = new Drone(200, 100, 150, 25);
-		JLabel label = new JLabel(drone);
-
+		Drone drone = new Drone(100, 250);
 		
 		screen.add(plainBackground);
-		screen.addKeyListener(f);
-		label.requestFocus();
+		//screen.getContentPane().add(tracer);
 		
-
-		
-		plainBackground.add(label);
+		plainBackground.add(drone);
 		plainBackground.add(airplane);
 		plainBackground.add(airplane2);
 				
@@ -105,26 +39,9 @@ public class DroneGame //implements KeyListener
 		screen.setVisible(true);
 	}
 	
-	
-	public static void main(String[] args) throws IOException 
+	public static void main(String[] args) throws IOException
 	{
-		DroneGame persona5 = new DroneGame();
+		DroneGame MegaDimensionNeptuniaVII = new DroneGame();
 	}
-
-
-
-		//BufferedImage image = ImageIO.read(new File("Files/drone3.png"));
-	    //drone.add(label);
-		//drone.setSize(5, 5);
-		
-		plainBackground.add(label);
-		plainBackground.add(airplane);
-		
-		
-
-		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		screen.setSize(GAME_WIDTH , GAME_HEIGHT);
-		screen.setVisible(true);
-	}	
 
 }
