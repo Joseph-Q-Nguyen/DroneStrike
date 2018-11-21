d64142f76ec2127ef6cdd98708dcd5ae89e02882
Joseph <joseph.q.nguyen@sjsu.edu>
2018-11-21 11:33:49 -0800

    Updated Drone

--- Code/Drone.java
+++ Code/Drone.java
@@ -5,86 +5,128 @@ import java.awt.event.KeyListener;
 
 public class Drone extends JLabel implements KeyListener 
 {
-    
-    private Image image;
-    private int x, y;
-    
-    public Drone(int x, int y) 
-    {
-    	this.x = x;
-    	this.y = y;
-    	
-    	ImageIcon i = new ImageIcon("Files/drone2.png");
-    	image = i.getImage();
-        this.setPreferredSize(new Dimension(100, 80));
-       
-        addKeyListener(this);
-    }
+	private Image image;
+	private int droneX, droneY;
+	private Timer tUp, tDown, tRight, tLeft;
 
-    public void addNotify() 
-    {
-        super.addNotify();
-        requestFocus();
-    }
+	public Drone(int droneX, int droneY) 
+	{
+		this.droneX = droneX;
+		this.droneY = droneY;
 
-    public void paintComponent(Graphics g) 
-    {
-    	g.drawImage(image, x, y, 100, 80, null);
-    }
+		ImageIcon i = new ImageIcon("pics\\drone2.png");
+		image = i.getImage();
+		this.setPreferredSize(new Dimension(1, 1));
+//		this.setBounds(0, 0, 0, 0);
 
-    public void keyPressed(KeyEvent e) 
-    { 
-    	if(e.getKeyCode() == KeyEvent.VK_UP)
-        {
-        	changeY(-5);
-        	repaint();
-        }
-        if(e.getKeyCode() == KeyEvent.VK_DOWN)
-        {
-        	changeY(5);
-        	repaint();
-        }
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-        {
-        	changeX(5);
-        	repaint();
-        }
-        if(e.getKeyCode() == KeyEvent.VK_LEFT)
-        {
-        	changeX(-5);
-        	repaint();
-        }
-        
-    }
-    
-    public void keyReleased(KeyEvent e) 
-    { 
-    	
-    }
-    
-    public void keyTyped(KeyEvent e) 
-    {
-    	
-    }
+		addKeyListener(this);
 
-    public int getY()
-    {
-    	return y;
-    }
-    
-    public int getX()
-    {
-    	return x;
-    }
-    
-    public void changeX(int lateral)
-    {
-    	x += lateral;
-    }
-    
-    public void changeY(int vertical)
-    {
-    	y += vertical;
-    }
-    
+		int delay = 10;
+		tUp = new Timer(delay, event -> {
+			changeY(-10);
+			repaint();
+		});
+		tDown = new Timer(delay, event -> {
+			changeY(10);
+			repaint();
+		});
+		tRight = new Timer(delay, event -> {
+			changeX(10);
+			repaint();
+		});
+		tLeft = new Timer(delay, event -> {
+			changeX(-10);
+			repaint();
+		});
+	}
+	
+
+	public void addNotify() 
+	{
+		super.addNotify();
+		requestFocus();
+	}
+
+	public void paintComponent(Graphics g) 
+	{
+	//	Graphics2D g2 = (Graphics2D)g;
+		((Graphics2D)g).drawImage(image, droneX, droneY, 100, 80, this);
+	}
+
+	public void keyPressed(KeyEvent e) 
+	{ 
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			tUp.start();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			tDown.start();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			tRight.start();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			tLeft.start();
+		}
+
+	}
+
+	public void keyReleased(KeyEvent e) 
+	{ 
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			tUp.stop();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			tDown.stop();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			tRight.stop();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			tLeft.stop();
+		}
+	}
+
+	public void keyTyped(KeyEvent e) 
+	{
+
+	}
+
+	public int getDroneY()
+	{
+		return droneY;
+	}
+
+	public int getDroneX()
+	{
+		return droneX;
+	}
+
+	public void moveRight()
+	{
+		int delay = 10;
+		Timer t = new Timer(delay, event -> {
+			changeX(1);
+			repaint();
+		});
+		t.start();
+	}
+
+	public void changeX(int lateral)
+	{
+		droneX += lateral;
+	}
+
+	public void changeY(int vertical)
+	{
+		droneY += vertical;
+	}
+
 }
