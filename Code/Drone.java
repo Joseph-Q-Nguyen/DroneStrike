12aa7b641ed08c7604ea1ef2d3735f1898fbcbd5
micompany4 <42653786+micompany4@users.noreply.github.com>
2018-11-22 11:57:14 -0800

    Update Drone.java

--- Code/Drone.java
+++ Code/Drone.java
@@ -2,193 +2,177 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.geom.Area;
 
 public class Drone extends JLabel implements KeyListener 
 {
-    
-    private Image image;
-    private int x, y, height, width, xd, yd;
-    private Airplane emeny;
-    private boolean collision;
-    
-    public Drone(int x, int y) 
-    {
-    	this.x = x;
-    	this.y = y;
-    	//emeny = new Airplane(100, 100, 150, 25);
-    	collision = false;
-    	
-    	Rectangle r = new Rectangle(x, y, 100, 80);
-    	
-    	ImageIcon i = new ImageIcon("Files/drone2.png");
-    	image = i.getImage();
-    	height = 80;//image.getHeight(null);
-    	width = 100;//image.getWidth(null);
-    	
-    	xd = x + width;
-    	yd = y + height;
-    	
-    	//this.setBounds(new Rectangle(x, y, width, height));
-        this.setPreferredSize(new Dimension(0, 0));
-        addKeyListener(this);
-    }
+	private Image image;
+	private int x, y, width, height;
+	private Timer tUp, tDown, tRight, tLeft;
 
-    public void addNotify() 
-    {
-        super.addNotify();
-        requestFocus();
-    }
+	public Drone(int x, int y) 
+	{
+		this.x = y;
+		this.x = y;
 
-    public void paintComponent(Graphics g) 
-    {
-    	g.drawImage(image, x, y, width, height, null);
-    	//g.drawRect(x, y, width, height);
-    	//repaint();
-    	
-    }
+		ImageIcon i = new ImageIcon("Files\\drone2.png");
+		image = i.getImage();
+		this.setPreferredSize(new Dimension(1, 1));
+//		this.setBounds(0, 0, 0, 0);
 
-    public void reset()
-    {
-    	x = 50;
-    	y = 150;
-    }
-    
-    public void keyPressed(KeyEvent e) 
-    { 
-    	if(e.getKeyCode() == KeyEvent.VK_UP)
-        {
-        	changeY(-5);
-        	changeYD(-5);
-        	if(y <= 0)
+		this.width = image.getWidth(this);
+		this.height = image.getHeight(this);
+		
+		addKeyListener(this);
+
+		int delay = 10;
+		tUp = new Timer(delay, event -> {
+			changeY(-10);
+			
+        	
+			repaint();
+		});
+		tDown = new Timer(delay, event -> {
+			changeY(10);
+			repaint();
+		});
+		tRight = new Timer(delay, event -> {
+			changeX(10);
+			repaint();
+		});
+		tLeft = new Timer(delay, event -> {
+			changeX(-10);
+			repaint();
+		});
+	}
+	
+
+	public void addNotify() 
+	{
+		super.addNotify();
+		requestFocus();
+	}
+
+	public void paintComponent(Graphics g) 
+	{
+	//	Graphics2D g2 = (Graphics2D)g;
+		((Graphics2D)g).drawImage(image, x, y, width, height, this);
+		
+		//g.drawImage(image, x, y, width, height, this);
+	}
+
+	public void keyPressed(KeyEvent e) 
+	{ 
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			
+			if(y <= 0)
         	{
         		y = 0;
+        		
         	}
-        	if(yd <= 80)
-        	{
-        		yd = 80;
-        	}
-        	repaint();
-        }
-        if(e.getKeyCode() == KeyEvent.VK_DOWN)
-        {
-        	changeY(5);
-        	changeYD(5);
-        	if(y >= 375)
+			else
+			{
+				tUp.start();
+			}
+        	
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			
+			if(y >= 375)
         	{
         		y = 375;
         	}
-        	if(yd >= 435)
-        	{
-        		yd = 435;
-        	}
-        	repaint();
-        }
-        if(e.getKeyCode() == KeyEvent.VK_RIGHT)
-        {
-        	changeX(5);
-        	changeXD(5);
-        	if(x >= 875)
+			else
+			{
+				tDown.start();
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			
+			if(x >= 875)
         	{
         		x = 875;
         	}
-        	if(xd >= 975)
-        	{
-        		xd = 975;
-        	}
-        	repaint();
-        }
-        if(e.getKeyCode() == KeyEvent.VK_LEFT)
-        {
-        	changeX(-5);
-        	changeXD(-5);
-        	if(x <= 0)
+			else
+			{
+				tRight.start();
+			}
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			
+			if(x <= 0)
         	{
         		x = 0;
         	}
-        	if(xd <= 100)
-        	{
-        		xd = 100;
-        	}
-        	repaint();
-        }
-        if(e.getKeyCode() == KeyEvent.VK_R)
-        {
-        	reset();
-        }
-        
-    }
-    
-    public void keyReleased(KeyEvent e) 
-    { 
-    	
-    }
-    
-    public void keyTyped(KeyEvent e) 
-    {
-    	
-    }
+			else
+			{
+				tLeft.start();
+			}
+		}
 
-    public int getY()
-    {
-    	return y;
-    }
-    
-    public int getX()
-    {
-    	return x;
-    }
-    
-    public int getYD()
-    {
-    	return yd;
-    }
-    
-    public int getXD()
-    {
-    	return xd;
-    }
-    
-    public int getHeight()
-    {
-    	return height;
-    }
-    
-    public int getWidth()
-    {
-    	return width;
-    }
-    
-    public void changeX(int lateral)
-    {
-    	x += lateral;
-    }
-    
-    public void changeY(int vertical)
-    {
-    	y += vertical;
-    }
-    
-    public void changeYD(int crossY)
-    {
-    	yd += crossY;
-    }
-    
-    public void changeXD(int crossX)
-    {
-    	xd += crossX;
-    }
-    
-    public boolean getCollision()
-    {
-    	return collision;
-    }
-    
-    public void setCollision(boolean c)
-    {
-    	collision = c;
-    }
-    
-    
-    
+	}
+
+	public void keyReleased(KeyEvent e) 
+	{ 
+		if (e.getKeyCode() == KeyEvent.VK_UP)
+		{
+			tUp.stop();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+		{
+			tDown.stop();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+		{
+			tRight.stop();
+		}
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+		{
+			tLeft.stop();
+		}
+	}
+
+	public void keyTyped(KeyEvent e) 
+	{
+
+	}
+
+	public int getX()
+	{
+		return x;
+	}
+
+	public int getY()
+	{
+		return y;
+	}
+
+	public void reset()
+	{
+		x = 50;
+		y = 250;
+	}
+	
+	public void changeX(int lateral)
+	{
+		x += lateral;
+		if(x == 0)
+		{
+			x = 0;
+		}
+	}
+
+	public void changeY(int vertical)
+	{
+		y += vertical;
+	}
+
+	public boolean checkBounds()
+	{
+		return true;
+	}
+	
 }
