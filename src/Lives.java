8e3437452861e259ddb802053fb67ee6c5e6b35d
micompany4 <42653786+micompany4@users.noreply.github.com>
2018-11-26 19:06:13 -0800

    Update Lives.java

--- src/Lives.java
+++ src/Lives.java
@@ -1,11 +1,39 @@
 import javax.swing.JLabel;
 
-public class Lives extends JLabel {
+public class Lives extends JLabel 
+{
 	
 	JLabel label = this;
+	private int lives;
 	
-	public Lives() {
-		label.setText("Lives Left: 3 out of 3");
+	public Lives() 
+	{
+		lives = 3;
+		label.setText("Lives Left: " + lives + " out of 3");
 	}
 
+	public void lost()
+	{
+		lives--;
+		label.setText("Lives Left: " + lives + " out of 3");
+		
+	}
+	
+	public void won()
+	{
+		lives = 3;
+		label.setText("Lives Left: " + lives + " out of 3");
+	}
+	
+	public void getALife()
+	{
+		lives = 3;
+		label.setText("Lives Left: " + lives + " out of 3");
+	}
+	
+	public int getLives()
+	{
+		return lives;
+	}
+	
 }
