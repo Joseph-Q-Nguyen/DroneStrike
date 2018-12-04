23d0cf0f45ed81f7ebd140fcb52dc0c1ba4882b7
Adam Golab <adam.golab@sjsu.edu>
2018-12-03 18:42:51 -0800

    Add files via upload

--- src/DroneGame.java
+++ src/DroneGame.java
@@ -1,6 +1,10 @@
+import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 import javax.swing.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 
 // Joseph's note: anything added to be displayed on screen must be added to the Moving Plain
@@ -12,38 +16,28 @@ public class DroneGame
 	private Random r;
 	
 	private Drone drone;
-	private Airplane f15, tomcat, dog;
-	
-	private Airplane[] airforce = new Airplane[6];
-	
+	private Airplane f15, tomcat;
+
 	public DroneGame() throws IOException, InterruptedException
-	{
+	{	
 		JFrame screen = new JFrame("Drone Strike");
 		MovingPlain plainBackground = new MovingPlain();
 		r = new Random();
 		Lives lives = new Lives();
 
-		drone = new Drone(50, 150);
-		
-		f15 = new Airplane(850, r.nextInt(450), 150, 25, false, drone);
+		f15 = new Airplane(850, r.nextInt(450), 150, 25);
 		JLabel airplane = new JLabel(f15);
 		airplane.setBounds(0, 0, 1, 1);
-		tomcat = new Airplane(800, r.nextInt(450), 150, 25, false, drone);
+		tomcat = new Airplane(800, r.nextInt(450), 150, 25);
 		JLabel airplane2 = new JLabel(tomcat);
 		airplane2.setBounds(0, 0, 1, 1);
-		
-		
-		
-		airforce[0] = f15;
-		airforce[1] = tomcat;
-		//airforce[2] = airplane3;
-		
 		Time time = new Time();
 		Scores score = new Scores();
 
-		
+		drone = new Drone(50, 150);
 
 		screen.add(plainBackground);
+		playSound();
 
 
 		time.setBounds(500, 390, 75, 75);
@@ -56,39 +50,37 @@ public class DroneGame
 		plainBackground.add(airplane2);
 		plainBackground.setLayout(null);
 
+
+
 		
+
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		screen.setSize(GAME_WIDTH , GAME_HEIGHT);
 		screen.setVisible(true);
+		
 
 		while(screen.isVisible()) 
 		{
 			System.out.println("");
-			for(int i = 0; i < 2; i++)
+			if(crashF())
 			{
-				if(airforce[i].crashF(drone))
-				{
-					lives.lost();
-					drone.reset();
-					airforce[i].reset();
-				}
-				
-				
+				playHitSound();
+				lives.lost();
+				drone.reset();
+				f15.reset();
 			}
-			
+			if(crashT())
+			{
+				playHitSound();
+				lives.lost();
+				drone.reset();
+				tomcat.reset();
+			}
 			if(lives.getLives() == 0)
 			{
 				lives.getALife();
 				time.timeReset();
 				score.gameOver();
-			}
-			if(time.getTime() == 0)
-			{
-				dog = new Airplane(900, r.nextInt(450), 150, 25, true, drone);
-				JLabel airplane3 = new JLabel(dog);
-				airplane3.setBounds(0, 0, 1, 1);
-				plainBackground.add(airplane3);
-				
 			}
 			if(score.getWinner())
 			{
@@ -101,12 +93,65 @@ public class DroneGame
 		}
 	}
 
+	public boolean crashF() {
+		//bottom of plane
+		if (f15.getY()+20 < drone.getYs()-drone.getHeight()
+				|| f15.getY()-f15.getIconHeight()-50 > drone.getYs()) {
+			//top of plane
+			return false;
+		}
+		//back of plane
+		if (f15.getX()+f15.getIconWidth()-50 < drone.getXs()-drone.getHeight() 
+				|| f15.getX()-f15.getIconHeight() > drone.getXs()+drone.getWidth()) {
+			return false;
+		}
+		return true;
+	}
 
-	
+	public boolean crashT()
+	{
+		if (tomcat.getY()+20 < drone.getYs()-drone.getHeight()
+				|| tomcat.getY()-tomcat.getIconHeight()-50 > drone.getYs()) {
+			//top of plane
+			return false;
+		}
+		//back of plane
+		if (tomcat.getX()+tomcat.getIconWidth()-50 < drone.getXs()-drone.getHeight() 
+				|| tomcat.getX()-tomcat.getIconHeight() > drone.getXs()+drone.getWidth()) {
+			return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
 		DroneGame MegaDimensionNeptuniaVII = new DroneGame();
-		//System.out.println(drone.getWidth());
+	}
+	
+	public void playHitSound() {
+	    try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/adamgolab/Desktop/151project/sounds/canon.wav").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.start();
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
+	}
+	
+	
+	public void playSound() {
+	    try {
+	        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("/Users/adamgolab/Desktop/151project/sounds/mario.wav").getAbsoluteFile());
+	        Clip clip = AudioSystem.getClip();
+	        clip.open(audioInputStream);
+	        clip.loop(Clip.LOOP_CONTINUOUSLY);
+	        Thread.sleep(500);
+	    } catch(Exception ex) {
+	        System.out.println("Error with playing sound.");
+	        ex.printStackTrace();
+	    }
 	}
 
 }
