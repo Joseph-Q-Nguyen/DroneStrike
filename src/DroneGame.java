8a1786feae5a9bb9a0d27d0249a2bd231d9644d1
micompany4 <42653786+micompany4@users.noreply.github.com>
2018-12-03 17:45:45 -0800

    Update DroneGame.java

--- src/DroneGame.java
+++ src/DroneGame.java
@@ -11,10 +11,11 @@ public class DroneGame
 
 	private Random r;
 	
-	private static Drone drone;
-	private static Airplane f15, tomcat;
-	public int colCount = 3;
-
+	private Drone drone;
+	private Airplane f15, tomcat, dog;
+	
+	private Airplane[] airforce = new Airplane[6];
+	
 	public DroneGame() throws IOException, InterruptedException
 	{
 		JFrame screen = new JFrame("Drone Strike");
@@ -22,16 +23,25 @@ public class DroneGame
 		r = new Random();
 		Lives lives = new Lives();
 
-		f15 = new Airplane(850, r.nextInt(450), 150, 25);
+		drone = new Drone(50, 150);
+		
+		f15 = new Airplane(850, r.nextInt(450), 150, 25, false, drone);
 		JLabel airplane = new JLabel(f15);
 		airplane.setBounds(0, 0, 1, 1);
-		tomcat = new Airplane(800, r.nextInt(450), 150, 25);
+		tomcat = new Airplane(800, r.nextInt(450), 150, 25, false, drone);
 		JLabel airplane2 = new JLabel(tomcat);
 		airplane2.setBounds(0, 0, 1, 1);
+		
+		
+		
+		airforce[0] = f15;
+		airforce[1] = tomcat;
+		//airforce[2] = airplane3;
+		
 		Time time = new Time();
 		Scores score = new Scores();
 
-		drone = new Drone(50, 150);
+		
 
 		screen.add(plainBackground);
 
@@ -46,9 +56,7 @@ public class DroneGame
 		plainBackground.add(airplane2);
 		plainBackground.setLayout(null);
 
-
-
-
+		
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		screen.setSize(GAME_WIDTH , GAME_HEIGHT);
 		screen.setVisible(true);
@@ -56,7 +64,19 @@ public class DroneGame
 		while(screen.isVisible()) 
 		{
 			System.out.println("");
-			if(crashF())
+			for(int i = 0; i < 2; i++)
+			{
+				if(airforce[i].crashF(drone))
+				{
+					lives.lost();
+					drone.reset();
+					airforce[i].reset();
+				}
+				
+				
+			}
+			
+			/*if(crashF())
 			{
 				lives.lost();
 				drone.reset();
@@ -68,15 +88,30 @@ public class DroneGame
 				drone.reset();
 				tomcat.reset();
 			}
+			if(crashD())
+			{
+				lives.lost();
+				drone.reset();
+				dog.reset();
+			}*/
 			if(lives.getLives() == 0)
 			{
 				lives.getALife();
 				time.timeReset();
 				score.gameOver();
 			}
+			if(time.getTime() == 0)
+			{
+				dog = new Airplane(900, r.nextInt(450), 150, 25, true, drone);
+				JLabel airplane3 = new JLabel(dog);
+				airplane3.setBounds(0, 0, 1, 1);
+				plainBackground.add(airplane3);
+				
+			}
 			if(score.getWinner())
 			{
 				lives.won();
+				time.timeReset();
 				drone.reset();
 				f15.reset();
 				
@@ -84,7 +119,8 @@ public class DroneGame
 		}
 	}
 
-	public boolean crashF() {
+	public boolean crashF() 
+	{
 		//bottom of plane
 		if (f15.getY()+20 < drone.getYs()-drone.getHeight()
 				|| f15.getY()-f15.getIconHeight()-50 > drone.getYs()) {
@@ -93,7 +129,8 @@ public class DroneGame
 		}
 		//back of plane
 		if (f15.getX()+f15.getIconWidth()-50 < drone.getXs()-drone.getHeight() 
-				|| f15.getX()-f15.getIconHeight() > drone.getXs()+drone.getWidth()) {
+				|| f15.getX()-f15.getIconHeight() > drone.getXs()+drone.getWidth()) 
+		{
 			return false;
 		}
 		return true;
@@ -114,7 +151,22 @@ public class DroneGame
 		return true;
 	}
 
-	public static void detection()
+	public boolean crashD()
+	{
+		if (dog.getY()+20 < drone.getYs()-drone.getHeight()
+				|| dog.getY()-dog.getIconHeight()-50 > drone.getYs()) {
+			//top of plane
+			return false;
+		}
+		//back of plane
+		if (dog.getX()+dog.getIconWidth()-50 < drone.getXs()-drone.getHeight() 
+				|| dog.getX()-dog.getIconHeight() > drone.getXs()+drone.getWidth()) {
+			return false;
+		}
+		return true;
+	}
+	
+	public void detection()
 	{
 		System.out.print("X:" + drone.getX() + " ");
 		System.out.println("Y: " + drone.getY());
