4e3eaeb3795c8d662f29fb84b098240c1b0bd013
micompany4 <42653786+micompany4@users.noreply.github.com>
2018-12-03 18:49:03 -0800

    Update DroneGame.java

--- Code/DroneGame.java
+++ Code/DroneGame.java
@@ -1,10 +1,5 @@
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
-
 import javax.swing.*;
 
 
@@ -16,10 +11,11 @@ public class DroneGame
 
 	private Random r;
 	
-	private static Drone drone;
-	private static Airplane f15, tomcat;
-	public int colCount = 3;
-
+	private Drone drone;
+	private Airplane f15, tomcat, dog;
+	
+	private Airplane[] airforce = new Airplane[6];
+	
 	public DroneGame() throws IOException, InterruptedException
 	{
 		JFrame screen = new JFrame("Drone Strike");
@@ -27,16 +23,25 @@ public class DroneGame
 		r = new Random();
 		Lives lives = new Lives();
 
-		f15 = new Airplane(850, r.nextInt(450), 150, 25);
+		drone = new Drone(50, 150);
+		
+		f15 = new Airplane(850, r.nextInt(450), 150, 25, false, drone);
 		JLabel airplane = new JLabel(f15);
-		airplane.setBounds(850, r.nextInt(450), 150, 25);
-		tomcat = new Airplane(800, r.nextInt(450), 150, 25);
+		airplane.setBounds(0, 0, 1, 1);
+		tomcat = new Airplane(800, r.nextInt(450), 150, 25, false, drone);
 		JLabel airplane2 = new JLabel(tomcat);
-		airplane2.setBounds(850, r.nextInt(450), 150, 25);
+		airplane2.setBounds(0, 0, 1, 1);
+		
+		
+		
+		airforce[0] = f15;
+		airforce[1] = tomcat;
+		//airforce[2] = airplane3;
+		
 		Time time = new Time();
 		Scores score = new Scores();
 
-		drone = new Drone(50, 150);
+		
 
 		screen.add(plainBackground);
 
@@ -51,9 +56,7 @@ public class DroneGame
 		plainBackground.add(airplane2);
 		plainBackground.setLayout(null);
 
-
-
-
+		
 		screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		screen.setSize(GAME_WIDTH , GAME_HEIGHT);
 		screen.setVisible(true);
@@ -61,26 +64,29 @@ public class DroneGame
 		while(screen.isVisible()) 
 		{
 			System.out.println("");
-			if(crashF())
+			for(int i = 0; i < 2; i++)
 			{
-				lives.lost();
-				drone.reset();
-				f15.reset();
+				if(airforce[i].crashF(drone))
+				{
+					lives.lost();
+					drone.reset();
+					airforce[i].reset();
+				}
+				
+				
 			}
-			if(crashT())
-			{
-				lives.lost();
-				drone.reset();
-				tomcat.reset();
-			}
+			
 			if(lives.getLives() == 0)
 			{
 				lives.getALife();
+				time.timeReset();
 				score.gameOver();
 			}
+			
 			if(score.getWinner())
 			{
 				lives.won();
+				time.timeReset();
 				drone.reset();
 				f15.reset();
 				
@@ -88,42 +94,8 @@ public class DroneGame
 		}
 	}
 
-	public boolean crashF() {
-		//bottom of plane
-		if (f15.getY()+20 < drone.getYs()-drone.getHeight()
-				|| f15.getY()-f15.getIconHeight()-50 > drone.getYs()) {
-			//top of plane
-			return false;
-		}
-		//back of plane
-		if (f15.getX()+f15.getIconWidth()-50 < drone.getXs()-drone.getHeight() 
-				|| f15.getX()-f15.getIconHeight() > drone.getXs()+drone.getWidth()) {
-			return false;
-		}
-		return true;
-	}
 
-	public boolean crashT()
-	{
-		if (tomcat.getY()+20 < drone.getYs()-drone.getHeight()
-				|| tomcat.getY()-tomcat.getIconHeight()-50 > drone.getYs()) {
-			//top of plane
-			return false;
-		}
-		//back of plane
-		if (tomcat.getX()+tomcat.getIconWidth()-50 < drone.getXs()-drone.getHeight() 
-				|| tomcat.getX()-tomcat.getIconHeight() > drone.getXs()+drone.getWidth()) {
-			return false;
-		}
-		return true;
-	}
-
-	public static void detection()
-	{
-		System.out.print("X:" + drone.getX() + " ");
-		System.out.println("Y: " + drone.getY());
-	}
-
+	
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
 		DroneGame MegaDimensionNeptuniaVII = new DroneGame();
